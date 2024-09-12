83af73816d9e72b0e933cba72d308cec42d558bd
Dante <dyasui@uoregon.edu>
2024-09-11 18:16:37 -0700

    updated distreg.tex table

--- R/results.R
+++ R/results.R
@@ -11,7 +11,9 @@ data <- read_csv("data/data.csv")
 
 data <- data %>% 
   mutate(
+    # outcome: migration ratio of japanese to total new migrants
     y = mig_japn / mig_total,
+    # evacuation zone status
     ez = ifelse(
       STATENAM %in% c("Arizona", "California", "Oregon", "Washington"),
       1, 0
@@ -19,19 +21,34 @@ data <- data %>%
     ) # %>%
   # mutate(across(GilaRiver:HeartMt, ~ lm(., na.rm=TRUE)))
 
-lm1_1940 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1940)) 
-lm1_1950 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1950)) 
-lm1_1960 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1960)) 
-lm1_1970 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1970)) 
-lm1_1980 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1980)) 
-lm1_1990 <- lm(y ~ lm(campclosest_dist), data = data %>% filter(Year==1990)) 
-stargazer(lm1_1940, lm1_1950, lm1_1960, lm1_1970, lm1_1980, lm1_1990,  out = "tables/fig1.tex")
+lm1_1940 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1940)) 
+lm1_1950 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1950)) 
+lm1_1960 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1960)) 
+lm1_1970 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1970)) 
+lm1_1980 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1980)) 
+lm1_1990 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1990)) 
+# stargazer(lm1_1940, lm1_1950, lm1_1960, lm1_1970, lm1_1980, lm1_1990,  out = "tables/fig1.tex")
+stargazer(
+  lm1_1940, lm1_1950, lm1_1960, lm1_1970, lm1_1980, lm1_1990,
+  type = "text",
+  title = "Effects of Distance to Closest Camp on Japanese Migration by Decade",
+  column.labels = c("1940", "1950", "1960", "1970", "1980", "1990"),
+  dep.var.labels = "Ratio of Japanese American migrants to total migrants",
+  covariate.labels = c("Log distance to closest camp in meters", "Constant"),
+  omit.stat = c("ser", "f"),
+  no.space=TRUE
+)
 
-lm2_1940 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1940)) 
-lm2_1950 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1950)) 
-lm2_1960 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1960)) 
-lm2_1970 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1970)) 
-lm2_1980 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1980)) 
-lm2_1990 <- lm(y ~ lm(campclosest_dist) * ez, data = data %>% filter(Year==1990)) 
-stargazer(lm2_1940, lm2_1950, lm2_1960, lm2_1970, lm2_1980, lm2_1990,  out = "tables/fig2.tex")
+
+lm2_1940 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1940)) 
+lm2_1950 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1950)) 
+lm2_1960 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1960)) 
+lm2_1970 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1970)) 
+lm2_1980 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1980)) 
+lm2_1990 <- lm(y ~ log(campclosest_dist) * ez, data = data %>% filter(Year==1990)) 
+# stargazer(lm2_1940, lm2_1950, lm2_1960, lm2_1970, lm2_1980, lm2_1990,  out = "tables/fig2.tex")
+stargazer(
+  lm2_1940, lm2_1950, lm2_1960, lm2_1970, lm2_1980, lm2_1990,
+  type = "text"
+)
 
