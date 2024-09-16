a20f55b5bf01bf16bc72c27a4fed31bc0eaef67c
Dante <dyasui@uoregon.edu>
2024-09-16 16:05:38 -0700

    fixed reg tables

--- R/results.R
+++ R/results.R
@@ -1,23 +1,17 @@
 
 library(tidyverse)
-library(ipumsr)
-library(labelled)
-library(geos)
-library(data.table)
-library(sf)
 library(stargazer)
 
 data <- read_csv("data/data.csv") %>% 
   mutate(
     # outcome: migration percentage of japanese to total new migrants
-    y = mig_japn / mig_total * 100,
+    y = mig_japn / mig * 100,
     # evacuation zone status
     ez = ifelse(
-      STATENAM %in% c("Arizona", "California", "Oregon", "Washington"),
+      STATENAM_1990 %in% c("Arizona", "California", "Oregon", "Washington"),
       1, 0
     )
-    ) # %>%
-  # mutate(across(GilaRiver:HeartMt, ~ lm(., na.rm=TRUE)))
+    )  
 
 # data <- tibble(
 #   campclosest_dist = rnorm(1800000, 100000, n = 3141),
@@ -25,12 +19,12 @@ data <- read_csv("data/data.csv") %>%
 #   Year = sample(c(1940, 1950 ,1960, 1970, 1980, 1990), size = 3141, replace = TRUE)
 # )
 
-lm1_1940 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1940)) 
-lm1_1950 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1950)) 
-lm1_1960 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1960)) 
-lm1_1970 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1970)) 
-lm1_1980 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1980)) 
-lm1_1990 <- lm(y ~ log(campclosest_dist), data = data %>% filter(Year==1990)) 
+lm1_1940 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1940)) 
+lm1_1950 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1950)) 
+lm1_1960 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1960)) 
+lm1_1970 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1970)) 
+lm1_1980 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1980)) 
+lm1_1990 <- lm(y ~ logdisclosest, data = data %>% filter(Year==1990)) 
 stargazer(
   lm1_1940, lm1_1950, lm1_1960, lm1_1970, lm1_1980, lm1_1990,
   type = "text",
